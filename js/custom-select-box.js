8f3c4291f021b6f4f948e7558003232e6a40c6b1
Justin Jerez <jrz.09@hotmail.com>
2020-11-14 17:37:54 -0400

    Settings design finished

---
+++ js/custom-select-box.js
@@ -0,0 +1,52 @@
+// DOM Elements
+const optionsContainer = document.querySelector('#options-container')
+const optionsContainers = document.querySelectorAll('.settings__options-container')
+const optionSelected = document.querySelectorAll('#selected')
+
+console.log(optionSelected)
+console.log(optionsContainers)
+// Due the use of an async funtion to retrieve the data
+// This var initialize empty and then inside the async function 
+// It is filled
+var optionsList
+
+async function getCategories(url) {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        // Filling the select box with dinamic data
+        optionsContainer.innerHTML = `
+            ${data['trivia_categories'].map((category) => {
+            return `
+               <div class="settings__option">
+                    <input type="radio" name="category" id="${category['id']}" class="settings__radio" />
+                    <label for="${category['id']}">${category['name']}</label>
+               </div>`
+        }).join('')}
+        `
+
+        // Getting all category options
+        optionsList = document.querySelectorAll('.settings__option')
+
+        optionsList.addEventListener('click', () => {
+            console.log('Hola')
+        })
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// Adding event listener 
+optionSelected.forEach(option => {
+    option.addEventListener('click', () => {
+        optionsContainers.forEach(container => {
+            if (container != option.parentElement.firstElementChild)
+                container.classList.remove('active')
+        })
+        option.parentElement.firstElementChild.classList.toggle('active')
+    })
+})
+
+getCategories('https://opentdb.com/api_category.php')
