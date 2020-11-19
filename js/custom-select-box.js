0ed8ae66ad69d76a90ee8f92bd20b235135721f6
Justin Jerez <jrz.09@hotmail.com>
2020-11-19 18:51:15 -0400

    Settings section finished

--- js/custom-select-box.js
+++ js/custom-select-box.js
@@ -1,52 +1,50 @@
-// DOM Elements
-const optionsContainer = document.querySelector('#options-container')
-const optionsContainers = document.querySelectorAll('.settings__options-container')
-const optionSelected = document.querySelectorAll('#selected')
-
-console.log(optionSelected)
-console.log(optionsContainers)
-// Due the use of an async funtion to retrieve the data
-// This var initialize empty and then inside the async function 
-// It is filled
-var optionsList
-
-async function getCategories(url) {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        // Filling the select box with dinamic data
-        optionsContainer.innerHTML = `
-            ${data['trivia_categories'].map((category) => {
-            return `
-               <div class="settings__option">
-                    <input type="radio" name="category" id="${category['id']}" class="settings__radio" />
-                    <label for="${category['id']}">${category['name']}</label>
-               </div>`
-        }).join('')}
-        `
-
-        // Getting all category options
-        optionsList = document.querySelectorAll('.settings__option')
-
-        optionsList.addEventListener('click', () => {
-            console.log('Hola')
-        })
-    }
-    catch (error) {
-        console.log(error)
-    }
+const getCategories = () => {
+    fetch('https://opentdb.com/api_category.php')
+        .then(res => res.json())
+        .then(res => insertCategories(res))
+        .then(res => expandContainers())
+        .then(res => optionsListener())
+        .catch(res => console.log(res))
 }
 
-// Adding event listener 
-optionSelected.forEach(option => {
-    option.addEventListener('click', () => {
-        optionsContainers.forEach(container => {
-            if (container != option.parentElement.firstElementChild)
-                container.classList.remove('active')
-        })
-        option.parentElement.firstElementChild.classList.toggle('active')
-    })
-})
+const insertCategories = (data) => {
+    document.querySelector('#options-container').innerHTML =
+        `
+        ${data['trivia_categories'].map((category) => {
+            return `
+        <div class="settings__option">
+                <input type="radio" name="category" id="${category['id']}" class="settings__radio" />
+                <label for="${category['id']}">${category['name']}</label>
+        </div>`
+        }).join('')}
+    `
 
-getCategories('https://opentdb.com/api_category.php')
+    return document.querySelectorAll('.settings__option')
+}
+
+const expandContainers = () => {
+    document.querySelectorAll('.settings__selected').forEach(option => {
+        option.addEventListener('click', () => {
+            document.querySelectorAll('.settings__options-container')
+                .forEach(container => {
+                    if (container != option.previousElementSibling) {
+                        container.classList.remove('active')
+                    }
+                })
+            option.previousElementSibling.classList.toggle('active')
+        })
+    })
+}
+
+const optionsListener = () => {
+    document.querySelectorAll('.settings__option')
+        .forEach(option => {
+            option.addEventListener('click', () => {
+                option.parentElement.classList.remove('active')
+                option.parentElement.nextElementSibling.innerHTML = option.lastElementChild.innerHTML
+                option.parentElement.nextElementSibling.id = option.firstElementChild.id
+            })
+        })
+}
+
+getCategories()
