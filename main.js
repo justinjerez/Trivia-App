3d21b575fed7b82893d97c2f938d8fc48c94d0e1
Justin Jerez <jrz.09@hotmail.com>
2020-12-07 18:16:18 -0400

    Design and logic of result section finished

--- main.js
+++ main.js
@@ -80,6 +80,9 @@ const getQuestions = (url) => {
     fetch(url)
         .then(res => res.json())
         .then(res => {
+            // Print response
+            console.log(res)
+
             // Make sure the API return enough questions and no an error
             if (res['response_code'] == 0) {
                 // Go to the quiz page
@@ -106,13 +109,14 @@ const insertQuestions = (data) => {
 
         // Print questions
         return `<div class="question">
+        <span class="question__answers-correct">${question['correct_answer']}</span>
         <h2 class="heading-secondary question__title">${question['question']}</h2>
         <div class="question__answers">
-            ${answers.map((answer, index) => {
+        ${answers.map((answer, index) => {
             return `<div class="answer">
-                    <input type="radio" name="${question['question']}" id="${(answer + index)}">
-                    <label for="${answer + index}">${answer}</label> 
-                </div>`
+            <input type="radio" name="${question['question']}" id="${(answer + index)}">
+            <label for="${answer + index}">${answer}</label> 
+            </div>`
         }).join('')}
         </div>
         </div>`
@@ -126,8 +130,12 @@ document.querySelector('#next').addEventListener('click', (e) => {
     // Save the current question user is watching
     const current = document.querySelector('.current')
     const answers = current.lastElementChild.children
+    let checked = 0
+
     Array.prototype.forEach.call(answers, answer => {
         if (answer.firstElementChild.checked) {
+            checked += 1
+
             if (current.nextElementSibling) {
                 current.nextSibling.classList.add('current')
                 current.classList.remove('current')
@@ -135,6 +143,17 @@ document.querySelector('#next').addEventListener('click', (e) => {
             }
         }
     })
+
+    if (checked == 0) {
+        const alertMessage = document.querySelector('.alert__message')
+
+        alertMessage.parentElement.classList.add('active')
+        alertMessage.innerHTML = 'You have to mark an answer'
+
+        setTimeout(() => {
+            alertMessage.parentElement.classList.remove('active')
+        }, 3000)
+    }
 
     // If there exist another elemet after the current quesiton, go to it
 })
@@ -149,6 +168,52 @@ document.querySelector('#prev').addEventListener('click', (e) => {
         current.classList.remove('current')
     }
 })
+
+// Show results
+document.querySelector('#results').addEventListener('click', (e) => {
+    let counter = 0
+    let correctAnswers = 0
+    const results = document.querySelector('#results-section')
+    const questions = document.querySelectorAll('.question')
+
+    // Count questions that are filled
+    Array.prototype.forEach.call(questions, question => {
+        let answers = question.lastElementChild.children
+
+        Array.prototype.forEach.call(answers, answer => {
+            if (answer.firstElementChild.checked) {
+                // Increase the number of answers alredy answered
+                counter += 1
+
+                // Display results
+                results.firstElementChild.innerHTML += `
+                    <div class="results__question">
+                        <h2 class="results__question--title">${question.children[1].textContent}</h2>
+                        <div class="results__question--answers">
+                            <p class="results__question--answer">Correct answer: ${question.firstElementChild.textContent}</p>
+                            <p class="results__question--answer">Your answer: ${answer.lastElementChild.textContent}</p>
+                        </div>
+                    </div>
+                `
+
+                // Verify if the answer is correct
+                if (answer.lastElementChild.innerHTML == question.firstElementChild.innerHTML) {
+                    correctAnswers += 1
+                }
+            }
+        })
+    })
+
+    results.innerHTML += `
+        <h2 class="heading-secondary score">${correctAnswers} / ${counter}</h2>
+    `
+
+    if (counter == questions.length) {
+        document.querySelector('main.container').classList.remove('to-quiz')
+        document.querySelector('main.container').classList.add('to-results')
+    }
+})
+
 
 
 const mainContainer = document.querySelector('main.container')
